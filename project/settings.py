c67a054cc41ef111799ae4ecca8c241c098092d4
cptincredble <cptincredble@gmail.com>
2015-02-10 01:45:08 -0800

    freeze and deploy 4

--- project/settings.py
+++ project/settings.py
@@ -2,8 +2,7 @@
 
 import os
 
-REPO_NAME = "kevinshieh.github.io"  # Used for FREEZER_BASE_URL
-DEBUG = True
+DEBUG = False
 
 # Assumes the app is located in the same directory
 # where this file resides
@@ -19,9 +18,4 @@ PROJECT_ROOT = parent_dir(APP_DIR)
 FREEZER_DESTINATION = PROJECT_ROOT
 # Since this is a repo page (not a Github user page),
 # we need to set the BASE_URL to the correct url as per GH Pages' standards
-FREEZER_BASE_URL = "http://localhost/{0}".format(REPO_NAME)
 FREEZER_REMOVE_EXTRA_FILES = False  # IMPORTANT: If this is True, all app files
-                                    # will be deleted when you run the freezer
-FLATPAGES_MARKDOWN_EXTENSIONS = ['codehilite']
-FLATPAGES_ROOT = os.path.join(APP_DIR, 'pages')
-FLATPAGES_EXTENSION = '.md'
