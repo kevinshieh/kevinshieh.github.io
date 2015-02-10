ba6a42d197e0392f6593edd847782ab6103b2f44
cptincredble <cptincredble@gmail.com>
2015-02-10 01:34:57 -0800

    free and deploy attempt 2

--- project/views.py
+++ project/views.py
@@ -1,14 +1,14 @@
 from flask import render_template, url_for
-from app import app, pages
+from app import app
 
 @app.route('/')
 def about():
     return render_template('about.html')
 
-@app.route('/resume')
+@app.route('/resume/')
 def resume():
     return render_template('resume.html')
 
-@app.route('/contact')
+@app.route('/contact/')
 def contact():
     return render_template('contact.html')
