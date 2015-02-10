013996d4077f42e06e8fef0fcfbffb6d46de6439
cptincredble <cptincredble@gmail.com>
2015-02-10 01:21:29 -0800

    css + deploy attempt 1

--- project/views.py
+++ project/views.py
@@ -2,11 +2,6 @@ from flask import render_template, url_for
 from app import app, pages
 
 @app.route('/')
-def home():
-    return render_template('index.html')
-    # return 'Hello World!'
-
-@app.route('/about')
 def about():
     return render_template('about.html')
 
@@ -17,7 +12,3 @@ def resume():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-
-if __name__ == '__main__':
-    app.debug = True
-    app.run()
--- run.py
+++ run.py
@@ -2,6 +2,5 @@ import os
 from project import main
 
 if __name__ == '__main__':
-    # port = int(os.environ.get('PORT', 5000))
-    # main.app.run(host='0.0.0.0', port=port)
-    main.app.run()
+    port = int(os.environ.get('PORT', 5000))
+    main.app.run(host='0.0.0.0', port=port)
