d923bc6a1dabc950f60557be0e455857b83d4c9b
cptincredble <cptincredble@gmail.com>
2015-02-10 21:06:50 -0800

    so sexc

--- project/views.py
+++ project/views.py
@@ -4,11 +4,3 @@ from app import app
 @app.route('/')
 def about():
     return render_template('index.html')
-
-@app.route('/resume/')
-def resume():
-    return render_template('resume.html')
-
-@app.route('/contact/')
-def contact():
-    return render_template('contact.html')
